129c7172de90285c54dcda6eac5a8f1d1476312c
JavierR97 <javierrivera.webdev@gmail.com>
2021-11-20 20:05:05 -0500

    created a variable called pokemonList
    created 3 objects with name type and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Welcome to\nThe PokeDex')
+let pokemonList=[{name:"Bulbasaur", type:["Grass","Poison"], height: "0.7"},{name:"Charmander", type:"Fire", height: "0.6"},{name:"Squirtle", type:"Water", height: "0.5"}]
 
-let favoriteDogBreed = 'bull terrior';
-document.write(favoriteDogBreed)
+// bulbasaur name, type, and height
+console.log (pokemonList[0].name);
+console.log(pokemonList[0].type);
+console.log(pokemonList[0].height)
+
+// bulbasaur name, type, and height
+console.log (pokemonList[1].name);
+console.log(pokemonList[1].type);
+console.log(pokemonList[1].height)
+
+// bulbasaur name, type, and height
+console.log (pokemonList[2].name);
+console.log(pokemonList[2].type);
+console.log(pokemonList[2].height)
