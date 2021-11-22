35de6721aaceab2d9c50d2a8826c9d4500020739
JavierR97 <javierrivera.webdev@gmail.com>
2021-11-22 17:12:31 -0500

    created a for loop
    added for loop displays each pokemons name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-let pokemonList=[{name:"Bulbasaur", type:["Grass","Poison"], height: "0.7"},{name:"Charmander", type:"Fire", height: "0.6"},{name:"Squirtle", type:"Water", height: "0.5"}]
+let pokemonList=[
+{name:"Bulbasaur", type:["Grass","Poison"], height: "0.7"},
+{name:"Charmander", type:"Fire", height: "0.6"},
+{name:"Squirtle", type:"Water", height: "0.5"}
+];
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + " height:" + pokemonList[i].height + " ");
+}
 
 // bulbasaur name, type, and height
 console.log (pokemonList[0].name);
