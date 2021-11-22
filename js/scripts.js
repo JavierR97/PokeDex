474dd995ed53e7d1351880fd158b0e0b41a3f180
JavierR97 <javierrivera.webdev@gmail.com>
2021-11-22 18:17:04 -0500

    loop created
    created a loop that prints all pokemon and their height. and an if conditional
    that prints "This is the largest Gen 1 starter Pokemon." to bulbasaur

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 let pokemonList=[
-{name:"Bulbasaur", type:["Grass","Poison"], height: "0.7"},
-{name:"Charmander", type:"Fire", height: "0.6"},
-{name:"Squirtle", type:"Water", height: "0.5"}
+{name:"Bulbasaur", type:["Grass","Poison"], height: 0.7},
+{name:"</br>Charmander", type:"Fire", height: 0.6},
+{name:"</br>Squirtle", type:"Water", height: 0.5}
 ];
+// this loop prints all of pokemonLists items to the page
+// the if statment in this loop reads as if pokemonList height is
+// greater than 0.6 then print doc.write(" ") to browser
+
 for (let i = 0; i < pokemonList.length; i++){
   document.write(pokemonList[i].name + " height:" + pokemonList[i].height + " ");
+  if (pokemonList[i].height > 0.6) {
+  document.write(" This is the largest Gen 1 starter Pokemon. ");
+  }
 }
 
 // bulbasaur name, type, and height
