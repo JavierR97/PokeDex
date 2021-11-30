43c1f1a88b886cdd374884c7240efd3c397a57c2
JavierR97 <javierrivera.webdev@gmail.com>
2021-11-30 16:10:17 -0500

    added iffe and foreach

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,27 @@
+//IIFE
 let pokemonRepository = (function () {
-  let pokemonList = [{name:"Bulbasaur", type:["Grass","Poison"], height: 0.7},
+  let pokemonList = [
+  {name:"Bulbasaur", type:["Grass","Poison"], height: 0.7},
   {name:"Charmander", type:"Fire", height: 0.6},
   {name:"Squirtle", type:"Water", height: 0.5}];
+  function add(pokemon) {
+  pokemonList.push(pokemon);
+  }
+
+  function getAll(pokemon) {
+    return pokemonList;
+  }
+
   return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
-    }
+    add: add,
+    getAll: getAll
   };
 })();
 
-pokemonlist.forEach {
+// pokemonRepository returns an array of pokemonList
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write(pokemon.name + " type: " + pokemon.type + " height: " + pokemon.height + "</br>");
 });
 
-
-// for (let i = 0; i < pokemonList.length; i++){
-//   document.write(pokemonList[i].name + " height:" + pokemonList[i].height + " ");
-//   document.write("<br />")
-//   if (pokemonList[i].height > 0.6) {
-//   document.write(" This is the largest Gen 1 starter Pokemon. <br />");
-//   }
-// }
-//
-// //bulbasaur name, type, and height
-// console.log (pokemonList[0].name);
-// console.log(pokemonList[0].type);
-// console.log(pokemonList[0].height)
-//
-// //bulbasaur name, type, and height
-// console.log (pokemonList[1].name);
-// console.log(pokemonList[1].type);
-// console.log(pokemonList[1].height)
-//
-// //Sbulbasaur name, type, and height
-// console.log (pokemonList[2].name);
-// console.log(pokemonList[2].type);
-// console.log(pokemonList[2].height;
-//
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.add({ name: 'Pikachu' }));
