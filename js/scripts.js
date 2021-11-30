5cfd3180db73d1369ee1576dc7584d2b4c040fef
JavierR97 <javierrivera.webdev@gmail.com>
2021-11-29 19:18:11 -0500

    forEach loop added

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
 let pokemonList=[
 {name:"Bulbasaur", type:["Grass","Poison"], height: 0.7},
-{name:"</br>Charmander", type:"Fire", height: 0.6},
-{name:"</br>Squirtle", type:"Water", height: 0.5}
-];
-// this loop prints all of pokemonLists items to the page
-// the if statment in this loop reads as if pokemonList height is
-// greater than 0.6 then print doc.write(" ") to browser
+{name:"Charmander", type:"Fire", height: 0.6},
+{name:"Squirtle", type:"Water", height: 0.5}
+ ];
 
-for (let i = 0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + " height:" + pokemonList[i].height + " ");
-  if (pokemonList[i].height > 0.6) {
-  document.write(" This is the largest Gen 1 starter Pokemon. ");
-  }
-}
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + " type: " + pokemon.type + " height: " + pokemon.height + "</br>");
+});
 
-// bulbasaur name, type, and height
-console.log (pokemonList[0].name);
-console.log(pokemonList[0].type);
-console.log(pokemonList[0].height)
-
-// bulbasaur name, type, and height
-console.log (pokemonList[1].name);
-console.log(pokemonList[1].type);
-console.log(pokemonList[1].height)
-
-// bulbasaur name, type, and height
-console.log (pokemonList[2].name);
-console.log(pokemonList[2].type);
-console.log(pokemonList[2].height)
+// for (let i = 0; i < pokemonList.length; i++){
+//   document.write(pokemonList[i].name + " height:" + pokemonList[i].height + " ");
+//   document.write("<br />")
+//   if (pokemonList[i].height > 0.6) {
+//   document.write(" This is the largest Gen 1 starter Pokemon. <br />");
+//   }
+// }
+//
+// //bulbasaur name, type, and height
+// console.log (pokemonList[0].name);
+// console.log(pokemonList[0].type);
+// console.log(pokemonList[0].height)
+//
+// //bulbasaur name, type, and height
+// console.log (pokemonList[1].name);
+// console.log(pokemonList[1].type);
+// console.log(pokemonList[1].height)
+//
+// //Sbulbasaur name, type, and height
+// console.log (pokemonList[2].name);
+// console.log(pokemonList[2].type);
+// console.log(pokemonList[2].height)
