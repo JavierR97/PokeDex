34d2c0650a60666b9a73d3cff2b1ab7947a853b1
JavierR97 <javierrivera.webdev@gmail.com>
2021-12-05 19:00:22 -0500

    li, button created/ console log shows pokemon on click
    created a function(addListItem) that returns my pokemon list as a 'ul' of button list items when called on.

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,55 @@ let pokemonRepository = (function () {
   function add(pokemon) {
   pokemonList.push(pokemon);
   }
-
+//this function will return the pokemonList array when called
   function getAll(pokemon) {
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    //getting element that conects to an exsisting element
+    let pokemonList = document.querySelector(".pokemon-list");
+    // this creates a li element
+    let listElement = document.createElement("li");
+    //this creates a button element
+    let button = document.createElement("button");
+    // creates text inside of new element "button"
+    button.innerText = pokemon.name;
+    // assigning a class to element 'button'
+    button.classList.add("button-class");
+    // adding child to list element
+    listElement.appendChild(button);
+    // adding list element to pokemonList element
+    pokemonList.appendChild(listElement);
+    /*eventListener listests for a click
+    the function then console logs the showDetails function below */
+    button.addEventListener('click', function (event) {
+      console.log(showDetails(pokemon));
+   });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name + ' ' +pokemon.type + " " +pokemon.height)
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-// pokemonRepository returns an array of pokemonList
-//adds pikachu to array
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name:'pikachu', type:'electric', height: 0.3});
 
+
+pokemonRepository.add({ name:'pikachu', type:['electric'], height: 0.3});
+//logs the pokemonList array in the console
+console.log(pokemonRepository.getAll());
+// pokemonRepository returns an array of pokemonLis
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + " type: " + pokemon.type + " height: " + pokemon.height + "</br>");
+  pokemonRepository.addListItem(pokemon);
 });
+
 
 
 
@@ -41,7 +71,7 @@ pokemonRepository.getAll().forEach(function(pokemon) {
 
 // console.log(pokemonRepository.getAll());
 
-
+// pokemonRepository.add({ name:'pikachu', type:['electric'], height: 0.3});
 
 // pokemonRepository.add({ name: 'Pikachu', type:'Electric', height:'0.3' });
 // pokemonRepository.getAll().forEach(function(pokemon) {
